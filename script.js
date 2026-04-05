1750a7e10be60743faaf1295ea59d3a4b8482f5e
Hangyul Park <hydrodddd@gmail.com>
2026-04-05 19:28:14 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,179 @@
+//elements
+var save = document.querySelector("#save")
+var bodytext = document.querySelector("#bodytext")
+var options = document.querySelector("#options")
+var line = document.querySelector("#line")
+var tools = document.querySelector("#tools")
+var optcon = document.querySelector("#optionContainer")
+var optionbg = document.querySelector("#optionbg")
+var format = document.querySelectorAll(".format")
+var [bold, italic, underline] = format
+
+//data
+let boldInstances = new Set()
+let italicInstances = new Set()
+let underlineInstances = new Set()
+
+//event listeners
+save.addEventListener("click", (e) => {
+    document.cookie = bodytext.innerHTML
+
+    save.style.backgroundColor = "rgb(212, 254, 164)"
+    bodytext.style.backgroundColor = "rgb(236, 255, 231)"
+    bodytext.style.borderColor = "rgb(212, 254, 164)"
+    setTimeout(() => {
+        save.style.backgroundColor = "rgb(254, 239, 164)"
+        bodytext.style.backgroundColor = "rgb(255, 249, 231)"
+        bodytext.style.borderColor = "rgb(254, 239, 164)"},
+    400)
+})
+
+options.addEventListener("mouseover", (e) => {
+    optionbg.style.backgroundColor = "rgb(254, 239, 164)"
+    optionbg.style.height = "80px"
+    tools.style.top = "-20px"
+    tools.style.opacity = "1"
+
+    tools.style.visibility = "visible"
+
+    optcon.style.pointerEvents = "auto"
+    bold.style.pointerEvents = "auto"
+    italic.style.pointerEvents = "auto"
+    underline.style.pointerEvents = "auto"
+    open = true
+})
+
+optcon.addEventListener("mouseout", (e) => {
+    if(!open || optcon.contains(e.relatedTarget)) return
+
+    optionbg.style.backgroundColor = "transparent"
+    optionbg.style.height = "20px"
+    tools.style.top = "-40px"
+
+    setTimeout(()=>{tools.style.visiblity = "hidden";tools.style.opacity = "0"}, 100);
+
+    optcon.style.pointerEvents = "none"
+    bold.style.pointerEvents = "none"
+    italic.style.pointerEvents = "none"
+    underline.style.pointerEvents = "none"
+    open = false
+})
+
+bold.addEventListener("click", (e) => {
+    let [start, end] = formatRange()
+
+    for(i = start; i < end; i++){
+        boldInstances.add(i)
+    }
+
+    loadEffects()
+})
+
+italic.addEventListener("click", (e) => {
+    let [start, end] = formatRange()
+
+    for(i = start; i < end; i++){
+        italicInstances.add(i)
+    }
+
+    loadEffects()
+})
+
+underline.addEventListener("click", (e) => {
+    let [start, end] = formatRange()
+    
+    for(i = start; i < end; i++){
+        underlineInstances.add(i)
+    }
+
+    loadEffects()
+})
+
+//pageload
+bodytext.innerHTML = document.cookie
+
+//internal functions
+function loadEffects(){
+    let text = getPlainText(bodytext.innerHTML)
+    const plainText = text
+
+    //how much formatting was inserted
+    let previous = 0
+
+    for(i = 0; i < plainText.length; i++){
+        if(boldInstances.has(i)){
+            text = text.slice(0,previous+i) + "<b>" + plainText.charAt(i) + 
+            "</b>" + text.slice(previous+i+1)
+            previous += 3
+        }
+        if(italicInstances.has(i)){
+            text = text.slice(0,previous+i) + "<i>" + plainText.charAt(i) + 
+            "</i>" + text.slice(previous+i+1)
+            previous += 3
+        }
+        if(underlineInstances.has(i)){
+            text = text.slice(0,previous+i) + "<u>" + plainText.charAt(i) + 
+            "</u>" + text.slice(previous+i+1)
+            previous += 3
+        }
+
+        text = text.slice(0,previous+i) + `<p id="t${i}">` + plainText.charAt(i) + 
+        "</p>" + text.slice(previous+i+1)
+        previous += 14 + i.toString().length
+
+        if(boldInstances.has(i)){
+            previous += 4
+        }
+        if(italicInstances.has(i)){
+            previous += 4
+        }
+        if(underlineInstances.has(i)){
+            previous += 4
+        }
+    }
+
+    bodytext.innerHTML = text
+}
+
+function getPlainText(text){
+    text = text.replaceAll("<b>", "")
+    text = text.replaceAll("</b>", "")
+    text = text.replaceAll("<i>", "")
+    text = text.replaceAll("</i>", "")
+    text = text.replaceAll("<u>", "")
+    text = text.replaceAll("</u>", "")
+    text = text.replaceAll("</p>", "")
+    let i = 0
+    while(text.indexOf(`<p id="t${i}">`) >= 0){
+        text = text.replace(`<p id="t${i}">`, "")
+        i++
+    }
+    return text
+}
+
+function formatRange(){
+    let start, end
+    let sel = window.getSelection()
+    if(bodytext.innerHTML == getPlainText(bodytext.innerHTML)){
+        if(sel.direction == "forward"){
+        start = (sel.direction == "forward")
+                ? sel.anchorOffset
+                : sel.focusOffset
+        end = (sel.direction == "forward")
+                ? sel.focusOffset
+                : sel.anchorOffset
+        }
+    }
+    else{
+        start = parseInt(((sel.direction == "forward")
+                        ? sel.anchorNode
+                        : sel.focusNode)
+                        .parentElement.id.slice(1))
+        end = parseInt(((sel.direction == "forward")
+                        ? sel.focusNode
+                        : sel.anchorNode)
+                        .parentElement.id.slice(1)) + 1
+    }
+
+    return [start, end]
+}
